093c7a14fb9617aa917ff7b48fc3284175241cc6
Hevski <heathercmalloch@gmail.com>
2019-10-12 15:24:19 +0100

    added map to app

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+  map: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initialiseMap();
+  }
+
+  initialiseMap() {
+    this.map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      target: 'map',
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+}
+
+}
