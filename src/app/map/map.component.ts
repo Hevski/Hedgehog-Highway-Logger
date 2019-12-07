fc7e47a96e2432e7c9dccb8aa940ded5c641e6ca
Hevski <heathercmalloch@gmail.com>
2019-12-07 16:37:38 +0000

    map is back

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -27,53 +27,56 @@ export class MapComponent implements OnInit {
   ngOnInit() {
     this.initialiseMap();
     this.addInteractions();
-    this.raster = new TileLayer({source: new OSM()});
     this.typeSelect = document.getElementById('type');
-    this.vectorSource = new VectorSource();
-    this.vectorLayer = new VectorLayer({
-      source: this.vectorSource,
-      style: new Style({
-        fill: new Fill({
-          color: 'rgba(255, 255, 255, 0.2)'
-        }),
-        stroke: new Stroke({
-          color: '#ffcc33',
-          width: 2
-        }),
-        image: new CircleStyle({
-          radius: 7,
-          fill: new Fill({
-            color: '#ffcc33'
-          })
-        })
-      })
-    });
-    // this.map.addLayer(this.vectorLayer);
-    // this.vectorLayer.setZIndex(999);
+    console.log(this.typeSelect);
   }
-
+    
+    // style: new Style({
+      //   fill: new Fill({
+        //     color: 'rgba(255, 255, 255, 0.2)'
+        //   }),
+        //   stroke: new Stroke({
+          //     color: '#ffcc33',
+          //     width: 2
+          //   }),
+          //   image: new CircleStyle({
+            //     radius: 7,
+            //     fill: new Fill({
+              //       color: '#ffcc33'
+              //     })
+              //   })
+              // })
+              
+              // this.map.addLayer(this.vectorLayer);
+              // this.vectorLayer.setZIndex(999);
+            
   initialiseMap() {
     const woodilee = [-4.14291973, 55.93130112,];
     const woodileeWebMercator = fromLonLat(woodilee);
-
+    this.vectorSource = new VectorSource();
+    const raster = new TileLayer({ source: new OSM() });
+    const vectorLayer = new VectorLayer({
+      source: this.vectorSource,
+    });
+    
     this.map = new Map({
-      layers:[this.raster, this.vectorLayer],
+      layers:[raster, vectorLayer],
       target: 'map',
       view: new View({
         center: woodileeWebMercator,
         zoom: 15,
       })
     });
-   }
-   
+  }
+  
   addInteractions() {
-   this.draw = new Draw({
-     source: this.vectorSource,
-     type: this.typeSelect.value
-   });
-   this.map.addInteraction(this.draw);
-   this.snap = new Snap({ source: this.vectorSource });
-   this.map.addInteraction(this.snap);
- }
+    this.draw = new Draw({
+      source: this.vectorSource,
+      type: this.typeSelect.value
+    });
+    this.map.addInteraction(this.draw);
+    this.snap = new Snap({ source: this.vectorSource });
+    this.map.addInteraction(this.snap);
+  }
 }
-
+          
